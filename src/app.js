5542e43aa8352b10fbb977dd02bafde9f743d4fe
Tenkorang Julius Anaafi <tenkorangjulius@gmail.com>
2026-03-05 02:54:57 +0000

    Completed Task Manager API with authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
